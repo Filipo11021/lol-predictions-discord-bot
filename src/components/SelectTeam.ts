3c0215a4f7fe2250552945cfb5536c6bf6060d11
Filipo <filipfilip280@gmail.com>
2024-01-28 00:41:30 +0100

    fix: exclude createdAt from team type

--- src/components/SelectTeam.ts
+++ src/components/SelectTeam.ts
@@ -60,7 +60,7 @@ function buildTeamSelects(events: EventT[]) {
 	return selects
 }
 
-function createTeamIfNotExist({ code, image, name }: Omit<Team, 'gameIds'>) {
+function createTeamIfNotExist({ code, image, name }: Omit<Team, 'gameIds' | "createdAt">) {
 	return db.team.upsert({
 		where: {
 			code,
